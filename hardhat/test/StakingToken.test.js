108fad1e98ae13a5d8e49de0b23ed33e6a15fe51
Francisco <jscriptcoder@gmail.com>
2023-02-15 12:07:47 +0100

    Added new test to hardhat

--- hardhat/test/StakingToken.test.js
+++ hardhat/test/StakingToken.test.js
@@ -4,15 +4,33 @@ const { ethers } = require('hardhat')
 const deploy = require('../scripts/deploy')
 
 describe('StakingToken', () => {
-  it('Should be possible to stake TOKs', async () => {
+  it('Should be possible to stake TOKs and unstake SATs', async () => {
     const accounts = await ethers.getSigners()
-    const { stakingToken } = await deploy()
+    const { sampleToken, stakingToken } = await deploy()
 
-    const tx = await stakingToken.stake(50)
+    const initialBalanceTOKs = await sampleToken.balanceOf(accounts[0].address)
+    const amountToStake = 1e4
 
-    await expect(tx).to.emit(stakingToken, 'Transfer')
+    // ====== Staking ====== //
+    const tx1 = await stakingToken.stake(amountToStake)
 
-    const balance = await stakingToken.balanceOf(accounts[0].address)
-    expect(balance, 50).to.equal(50)
+    await expect(tx1).to.emit(stakingToken, 'Transfer')
+
+    const balanceSATs = await stakingToken.balanceOf(accounts[0].address)
+    const balanceTOKs = await sampleToken.balanceOf(accounts[0].address)
+
+    expect(balanceSATs).to.equal(amountToStake)
+    expect(balanceTOKs).to.equal(initialBalanceTOKs - amountToStake)
+
+    // ====== Unstaking ====== //
+    const tx2 = await stakingToken.unstake(balanceSATs)
+
+    await expect(tx2).to.emit(stakingToken, 'Transfer')
+
+    const newBalanceSATs = await stakingToken.balanceOf(accounts[0].address)
+    const newBalanceTOKs = await sampleToken.balanceOf(accounts[0].address)
+
+    expect(newBalanceSATs).to.equal(0)
+    expect(newBalanceTOKs).to.equal(initialBalanceTOKs)
   })
 })
